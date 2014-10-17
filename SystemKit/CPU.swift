6fbd8d4bbd69c83be84ccd629d310d920405fee7
beltex <cse93251@cse.yorku.ca>
2014-10-17 14:38:03 -0400

    Add basic CPU.loadAvg()

--- SystemKit/CPU.swift
+++ SystemKit/CPU.swift
@@ -14,6 +14,24 @@ public class CPU {
     //--------------------------------------------------------------------------
     
     
+    /**
+    Get the system load average.
+    
+    - Can get stat from cl with w or uptime as well
+    
+    http://en.wikipedia.org/wiki/Load_(computing)
+    */
+    public func loadAvg() -> [Double] {
+        // TODO: Round to two decmial places
+        
+        var avg = [Double](count: 3, repeatedValue: 0)
+        
+        getloadavg(&avg, Int32(sizeof(Double) * 3))
+        
+        return avg
+    }
+    
+    
     public func numCores() -> Int {
         return 0
     }
