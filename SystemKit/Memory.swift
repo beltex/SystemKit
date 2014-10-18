109b6e34b352e805bb5aeac513bd80755de4a8ff
beltex <cse93251@cse.yorku.ca>
2014-10-18 18:48:30 -0400

    Add unit option for Memory class

--- SystemKit/Memory.swift
+++ SystemKit/Memory.swift
@@ -10,6 +10,23 @@ public class Memory {
     
     
     /**
+    Unit options for method data returns.
+    
+    TODO: Move this to a central place
+    */
+    public enum Unit : Double {
+        // For going from byte to -
+        case Byte     = 1
+        case Kilobyte = 1024
+        case Megabyte = 1048576
+        case Gigabyte = 1073741824
+    }
+    
+    
+    private let memsize_t_size : size_t = UInt(sizeof(UInt64))
+    
+    
+    /**
     Get the default page size for the system.
     
     - Can check this via pagesize shell command as well
@@ -17,37 +34,39 @@ public class Memory {
     
     :returns: System default page size in bytes
     */
-    public func pageSize() -> UInt {
+    public func pageSize(unit : Unit = Unit.Byte) -> Double {
         var port      : mach_port_t = mach_host_self()
         var pageSize  : vm_size_t   = 0
         
         // Check return if debug on?
         host_page_size(port, &pageSize)
         
-        return pageSize
+        return Double(pageSize) / unit.rawValue
     }
     
     
     /**
     Get the physical size of memory for this machine.
     
+    
+    :params: Optional unit value for return. Defaults to GB.
     :returns: The size as bytes
     */
-    public func physicalSize() -> UInt64 {
-        var opts = [CTL_HW, HW_MEMSIZE]
-        var size : size_t = UInt(sizeof(UInt64))
+    public func physicalSize(unit : Unit = Unit.Gigabyte) -> Double {
         var memsize : UInt64 = 0
+        var opts = [CTL_HW, HW_MEMSIZE]
+        var size = memsize_t_size
         
-        // Why does this return 8.5 GB?
-        var result = sysctl(&opts, 2, &memsize, &size, nil, 0)
+        let result = sysctl(&opts, 2, &memsize, &size, nil, 0)
         
-        if (result == -1) {
-            println("Error getting physical mem size")
+        if (result != KERN_SUCCESS) {
+            #if DEBUG
+                println("ERROR: \(__FUNCTION__) - \(result)")
+            #endif
+            
+            return 0
         }
-        else {
-            println("Mem size: \(memsize)")
-        }
         
-        return memsize
+        return Double(memsize) / unit.rawValue
     }
 }
