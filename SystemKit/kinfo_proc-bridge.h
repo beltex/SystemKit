a9d798ff5a1e3feb921670fb22073022872d13c7
beltex <beltex@users.noreply.github.com>
2014-11-24 13:52:02 -0500

    Use bridging header for kinfo-proc
    - This breaks framework use
    - In ref to - https://github.com/beltex/SystemKit/issues/12

---
+++ SystemKit/kinfo_proc-bridge.h
@@ -0,0 +1,10 @@
+
+
+#ifndef __SystemKit__kinfo_proc_bridge__
+#define __SystemKit__kinfo_proc_bridge__
+
+#include <stdio.h>
+
+void kinfo_for_pid(pid_t pid);
+
+#endif /* defined(__SystemKit__kinfo_proc_bridge__) */
